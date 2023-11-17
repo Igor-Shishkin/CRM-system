589ada95b04f850499a6faacf3fd22242707b0f3
Igor <igor.fizyk@gmail.com>
2023-11-17 16:35:47 +0100

    add angular project

---
+++ angular-crm/src/app/board-admin/board-admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-board-admin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './board-admin.component.html',
+  styleUrl: './board-admin.component.css'
+})
+export class BoardAdminComponent {
+
+}
---
+++ angular-crm/src/app/board-user/board-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-board-user',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './board-user.component.html',
+  styleUrl: './board-user.component.css'
+})
+export class BoardUserComponent {
+
+}
