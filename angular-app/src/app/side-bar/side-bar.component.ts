5d7755fef2752245ba0c46acd20caa82231136d0
Igor <igor.fizyk@gmail.com>
2023-12-02 14:32:26 +0100

    work with sidebar's style

--- angular-app/src/app/side-bar/side-bar.component.ts
+++ angular-app/src/app/side-bar/side-bar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SideBarComponent {
+
+    isDropdownActionVisible = false;
+    isDropdownHistoryVisible = false
+
+  toggleActionDropdown() {
+    this.isDropdownActionVisible = !this.isDropdownActionVisible;
+  }
+  toggleHistoryDropdown() {
+    this.isDropdownHistoryVisible = !this.isDropdownHistoryVisible;
+  }
 
 }
