56c3cc1c9c61b18acccecde4d792f4cef8bfa1b2
Igor <igor.fizyk@gmail.com>
2024-04-27 08:36:20 +0200

    refactoring Mock annotations for tests

--- server/src/test/java/com/crm/system/utils/mockAnnotations/WithMockCustomUserByUsername.java
+++ server/src/test/java/com/crm/system/utils/mockAnnotations/WithMockCustomUserByUsername.java
@@ -7,10 +7,6 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
-public @interface WithMockCustomUser {
+public @interface WithMockCustomUserByUsername {
     String username() default "user";
-    String email() default "test@gmail.com";
-    String password() default "password";
-    String[] roles() default {"USER"};
-    long id() default 1L;
 }
