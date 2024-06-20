2c4db89b03cc404680f9c179b3cf10ddf4125412
Igor <igor.fizyk@gmail.com>
2024-06-20 21:16:14 +0200

    update EntryType

--- server/src/main/java/com/crm/system/services/utils/logUtils/facadeForLogEntry/EntryType.java
+++ server/src/main/java/com/crm/system/services/utils/logUtils/facadeForLogEntry/EntryType.java
@@ -1,25 +1,43 @@
 package com.crm.system.services.utils.logUtils.facadeForLogEntry;
 
 public enum EntryType {
-    ADD_CLIENT {public String getTextForEntry(String fullName) {
-        return String.format("Client %s is created", fullName);
-    }},
-    SENT_CLIENT_TO_BLACKLIST {public String getTextForEntry(String fullName) {
-        return String.format("Client %s goes to blackList", fullName);
-    }},
-    RESTORE_CLIENT_FROM_BLACKLIST {public String getTextForEntry(String fullName) {
-        return String.format("Client %s is restored from blackList", fullName);
-    }},
-    EMAIL_IS_SENT() {public String getTextForEntry(String email) {
-        return String.format("Message sent. Email address: %s", email);
-    }},
-    SAVE_NEW_USER {public String getTextForEntry(String username) {
-        return String.format("User '%s' is added", username);
-    }},
-    DELETE_USER {public String getTextForEntry(String username) {
-        return String.format("User '%s' is deleted", username);
-    }};
 
-    public abstract String getTextForEntry(String s);
+    ADD_CLIENT (s -> "Client '"+ s +"' is created"),
+
+    SENT_CLIENT_TO_BLACKLIST (s -> "Client '" + s + "' goes to blackList"),
+
+    RESTORE_CLIENT_FROM_BLACKLIST (s -> "Client '"+ s + "' is restored from blackList"),
+
+    EMAIL_IS_SENT(s -> "Message sent. Email address: " + s),
+
+    SAVE_NEW_USER (s -> "User '" + s + "' is added"),
+
+    DELETE_USER (s -> "User '"+ s + "' is deleted"),
+
+    SIGN_AGREEMENT_FOR_ORDER(s -> "Signed an agreement with " + s),
+
+    CONFIRM_AGREEMENT (s -> "You have confirmed payment by" + s),
+
+    CANCEL_PAYMENT (s -> "You have canceled payment for " + s),
+
+    CANCEL_AGREEMENT (s -> "The contract with '" + s + "' clint was canceled"),
+
+    CREATE_NEW_ORDER (s -> "You have a new order from '" + s + "' client. Good job.");
+
+
+    private final EntryTextProvider textProvider;
+
+    EntryType(EntryTextProvider textProvider) {
+        this.textProvider = textProvider;
+    }
+
+    public String getTextForEntry(String s) {
+        return textProvider.getText(s);
+    }
+
+    @FunctionalInterface
+    private interface EntryTextProvider {
+        String getText(String s);
+    }
 
 }
